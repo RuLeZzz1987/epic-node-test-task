66f79d4166e96afd07cca823491dcdee3a8bb759
aduka <aduka@alteryx.com>
2019-08-06 13:57:49 +0300

    added tables to migrations: * game * player * developer

--- migrations/migrations_tables.sql
+++ migrations/migrations_tables.sql
@@ -1,6 +1,28 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS dummy (
-    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY
+CREATE TABLE IF NOT EXISTS player
+(
+    id           uuid                         default uuid_generate_v4() primary key,
+    last_seen    timestamp,
+    created_date timestamp           not null default now(),
+    display_name varchar(128) unique not null,
+    email        varchar(256) unique not null
 );
 
+create table if not exists game
+(
+    id           uuid                        default uuid_generate_v4() primary key,
+    developer_id uuid               not null,
+    publisher_id uuid               not null,
+    name         varchar(64)        not null,
+    slug         varchar(64) unique not null,
+    created_date timestamp          not null default now()
+);
+
+create table if not exists developer
+(
+    id           uuid                        default uuid_generate_v4() primary key,
+    slug         varchar(64) unique not null,
+    name         varchar(64)        not null,
+    created_date timestamp          not null default now()
+);
